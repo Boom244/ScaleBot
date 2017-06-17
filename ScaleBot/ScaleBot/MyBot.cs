512db3f9d90db6b762b23e989cbfada3ecce2c6d
Bolu <bolumsba@gmail.com>
2017-06-17 15:06:46 -0700

    Added the bot. made gitignore.

---
+++ ScaleBot/ScaleBot/MyBot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Discord;
+using Discord.Commands;
+namespace ScaleBot
+{
+	public class MyBot
+	{
+		DiscordClient discord;
+		public MyBot()
+		{
+			discord = new DiscordClient(x =>
+			{
+				x.LogLevel = LogSeverity.Info;
+				x.LogHandler = Log;
+			});
+			discord.ExecuteAndWait(async () =>
+			{
+				
+				try
+				{
+					string ReadText = File.ReadAllText("Token.txt");
+					await discord.Connect(ReadText, TokenType.Bot);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine("Could not find tokenfile.");
+				}
+			});
+		       
+		}	private void Log(object sender, LogMessageEventArgs e)
+		    {
+			Console.Write(e.Message);
+			}
+
+	}
+	}
+
---
+++ ScaleBot/ScaleBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScaleBot
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			MyBot bot = new MyBot();
+		}
+	}
+}
